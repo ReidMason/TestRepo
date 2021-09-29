3e53289059cd363cade653002d3ebbc75c05213e
Reid Mason <maddogshain132@gmail.com>
2021-09-29 20:22:33 +0100

    Added index route

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def greeting():
     return "Hello World!"
 
+@app.route('/index')
+def index():
+    return "This is the index page"
 
 app.run(host="0.0.0.0")
